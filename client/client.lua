37b7e9ea47ea34b2446b730c569376e1c9f57d0a
RexshackGaming <rexshackuk@gmail.com>
2024-10-09 08:22:53 +0100

    update version bump

--- client/client.lua
+++ client/client.lua
@@ -8,10 +8,7 @@ local function getAnimationForGender(playerPed)
     if isMale then
         return 'WORLD_HUMAN_DRINK_FLASK'
     else
-        return {
-            dict = 'amb_rest_drunk@world_human_drinking@female_a@idle_a',
-            anim = 'idle_a'
-        }
+        return { dict = 'amb_rest_drunk@world_human_drinking@female_a@idle_a', anim = 'idle_a' }
     end
 end
 
@@ -19,7 +16,7 @@ end
 local function LoadModel(modelHash)
     RequestModel(modelHash)
     while not HasModelLoaded(modelHash) do
-        Citizen.Wait(100)
+        Wait(100)
     end
 end
 
@@ -29,15 +26,14 @@ RegisterNetEvent('rsg-canteen:client:drink', function(amount)
         return
     else
         isBusy = not isBusy
-        SetCurrentPedWeapon(PlayerPedId(), joaat('weapon_unarmed'))
-        Citizen.Wait(100)
+        SetCurrentPedWeapon(cache.ped, joaat('weapon_unarmed'))
+        Wait(100)
         if not IsPedOnMount(cache.ped) and not IsPedInAnyVehicle(cache.ped) then
             
             local dict = 'amb_rest_drunk@world_human_drinking@female_a@idle_a'
             local anim = 'idle_a'
-            local ped = cache.ped
-            local coords = GetEntityCoords(ped)
-            local boneIndex = GetEntityBoneIndexByName(ped, "SKEL_R_HAND")
+            local coords = GetEntityCoords(cache.ped)
+            local boneIndex = GetEntityBoneIndexByName(cache.ped, "SKEL_R_HAND")
             local modelHash = GetHashKey("p_cs_canteen_hercule")
             LoadModel(modelHash)
             entity = CreateObject(modelHash, coords.x + 0.3, coords.y, coords.z, true, false, false)
@@ -45,13 +41,13 @@ RegisterNetEvent('rsg-canteen:client:drink', function(amount)
             SetEntityAlpha(entity, 255, false)
             Citizen.InvokeNative(0x283978A15512B2FE, entity, true)
             SetModelAsNoLongerNeeded(modelHash)
-            AttachEntityToEntity(entity, ped, boneIndex, 0.10, 0.09, -0.05, 306.0, 18.0, 0.0, true, true, false, true, 2, true)
+            AttachEntityToEntity(entity, cache.ped, boneIndex, 0.10, 0.09, -0.05, 306.0, 18.0, 0.0, true, true, false, true, 2, true)
 
             RequestAnimDict(dict)
             while not HasAnimDictLoaded(dict) do
                 Citizen.Wait(100)
             end
-            TaskPlayAnim(ped, dict, anim, 1.0, 1.0, -1, 31, 1.0, false, false, false)
+            TaskPlayAnim(cache.ped, dict, anim, 1.0, 1.0, -1, 31, 1.0, false, false, false)
         end
         Wait(5000)
         TriggerServerEvent("RSGCore:Server:SetMetaData", "thirst", RSGCore.Functions.GetPlayerData().metadata["thirst"] + amount)
